326c25d77fd32b6d0cef79955cbe79438cb2d65e
ArthurBuckingdale <41398578+ArthurBuckingdale@users.noreply.github.com>
2020-03-09 22:26:55 -0400

    Added in routine to compute the forces acting on the various bodies given to the problem

--- n-body-simulation/newtonian_gravity_nbody.m
+++ n-body-simulation/newtonian_gravity_nbody.m
@@ -1,4 +1,4 @@
-function [transit_timings,integration_error]=newtonian_gravity_nbody(varargin)
+function [transit_timings,integration_error]=newtonian_gravity_nbody(body_name,body_mass,init_state_vector,DEBUG)
 %The purpose of this script is to perform an n-body simulation for
 %planetary motion. It is going to be a re-write of my undergrad thesis. My
 %programming skills have increased a lot since and I think I can make a
@@ -9,51 +9,105 @@ function [transit_timings,integration_error]=newtonian_gravity_nbody(varargin)
 %will be used by a RL agent to solve for the mass of bodies in an exoplanet
 %system. It will also be able to play around with the number of bodies and
 %the orbital parameters such as SMA.
+%   inputs: body_name:cell array N length with N names of bodies
+%           body_mass:vector, N length with masses of bodies in body_name
+%           init_state_vector: 6xN matrix of (P,V) state vectors
+%   outputs:transit_timings......
+%           integration_error:double, contains the error from solving the
+%           ode's.
 
-%declaring some constants
-newtonsg = 6.647e-11;
 
-%let's now go ahead and solve a couple more cases and plot them
-[t1,y1] = ode45(@(t,y) odefcn(t,y,n), tspan, ic);
+% %let's now go ahead and solve a couple more cases and plot them
+% [t1,y1] = ode45(@(t,y) odefcn(t,y,n), tspan, ic);
+% %here is the odefun that is listed above.
+%     function dydt = odefcn(t,y,n)
+%         dydt = zeros(2,1);
+%         dydt(1) = y(2);
+%         dydt(2) = -y(1).^n-(2/t)*y(2);
+%     end
 
-
-%here is the odefun that is listed above.
-    function dydt = odefcn(t,y,n)
-        dydt = zeros(2,1);
-        dydt(1) = y(2);
-        dydt(2) = -y(1).^n-(2/t)*y(2);
-    end
-
+%% initialise the system
 %first, we are going to specify a struct for the planet variables
 %we want to do this to keep code legible and store all info related to a
-%body in a database style format.
+%body in a database style format. We are then going to compute all relative
+%distances and the
 
-for i=1:length(input_variable)
-    body_information(i).name=body_name(i);
+for i=1:length(body_name)
+    body_information(i).name=body_name{i};
     body_information(i).mass=body_mass(i);
-    body_information(i).position=initial_state_vector(1:3,i);
-    body_information(i).velocity=initial_state_vector(4:6,i);
+    body_information(i).position=init_state_vector(1:3,i);
+    body_information(i).velocity=init_state_vector(4:6,i);
+end
+kinetic_energy=calc_kinetic_energy(body_information);
+[relative_distance,grav_potential]=calc_relative_distances(body_information);
+initial_system_energy=sum(unique(grav_potential))+sum([kinetic_energy(:).value]);
+if DEBUG == 1
+    disp('information about all bodies input to the system')
+    unfold(body_information)
+    disp('Kinetic energy of all bodies in the system')
+    unfold(kinetic_energy)
+    disp('Relative distance and gravitational potential energy matrix of the system')
+    unfold(relative_distance)
+    disp(grav_potential)
+    fprintf('Initial enrgy contained in the system %d \n',initial_system_energy)
 end
 
-%compute kinetic and potential energies for error monitoring
-kinetic_energy=calc_kinetic_energy(body_information);
-%create a function to compute relative distances between bodies 
-relative_distance=calc_relative_distances(body_information);
-%create a function to compute gravitational potential energy
+%% prep the differential equation calculations
+%This section will contain the function that calculates the force which is
+%exerted on each object. This calculation is quite awful because the force
+%is a vector. Each component acting on each body needs to be calculated. We
+%are going to borrow part of the architecture from the
+%calc_relative_distances routine. We are not going to try and access the
+%information in here because we would be comparing a load of strings to
+%make sure we have the correct denominator for the force we are looking to
+%calculate.
+
+accel_of_gravity=calc_grav_accel(body_information);
+disp('acceleration due to gravity for the various components of the bodies')
+unfold(accel_of_gravity)
 
 
 
-    function scalar_gravitational_potential=calc_grav_potential_energy(body_information,relative_distance)
-        newtonsg = 6.647e-11;
-        for i=1:length(body_information)
-            for j=1:length(body_information)
-                scalar_gravitational_potential(i).value=newtonsg.*body_information
-            end
-        end
-        
-        
-        
-    end
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ n-body-simulation/utils/calc_grav_accel.m
@@ -0,0 +1,38 @@
+function accel_of_gravity=calc_grav_accel(body_information)
+%the purpose of this function is to copmute the first and second
+%derivative that are a result of the force of gravity. It will also
+%store the body we're on and the body we're looking at.
+%       Inputs: body_information
+%                   .name=string, of the body name
+%                   .mass=double, mass of the body
+%                   .velocity=3x1 vect double, velocity
+%                   .positiont=3x1 vect double, position of the body 
+%       Outputs:accel_of_gravity
+%                   .separation_distance: N-1 double vect with seperation
+%                   distances from each body
+%                   .acceleration: 3x(N-1) double vect, has the
+%                   acceleration due to gravity for (x,y,x) of each body
+%                   .body_im_in:N-1 cell string vect, contains the name of
+%                   the body we're standing in
+%                   .body_im_looking_at: N-1 cell string vect, containst
+%                   the list of bodies that i'm looking at.
+
+%define the format before entering the parfor loop
+% accel_of_gravity(1).separation_distance(1)=0.0;
+accel_of_gravity(1).acceleration(1:3,2)=zeros();
+% accel_of_gravity(1).body_im_in{1}='str';
+% accel_of_gravity(1).body_im_looking_at{1}='str';
+
+%entering the loop
+for i=1:length(body_information)
+    for j=1:length(body_information)
+        if j~=i
+            separation_distance=sqrt(norm(body_information(i).position-body_information(j).position)).^3;
+            accel_of_gravity(i).acceleration(1:3,j)=((body_information(j).mass).*([body_information(i).position]-[body_information(j).position]))./(separation_distance);
+            accel_of_gravity(i).body_im_in{j}=body_information(i).name;
+            accel_of_gravity(i).body_im_looking_at{j}=body_information(j).name;
+        end
+    end
+end
+
+end
